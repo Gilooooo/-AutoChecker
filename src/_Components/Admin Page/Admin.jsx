4d44c091e1182b76ccad336764617a0c46c2078e
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2023-11-22 15:12:35 +0800

    Admin.jsx

--- src/_Components/Admin Page/Admin.jsx
+++ src/_Components/Admin Page/Admin.jsx
@@ -1,10 +1,9 @@
 "use client";
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Link from 'next/link';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
 
-
-function AdminDashboard({clicked, setClicked}) {
+function AdminDashboard({ clicked, setClicked }) {
   const [facultiesOnline, setFaculties] = useState([]);
   const [studentsOnline, setStudents] = useState([]);
   const [login, setlogin] = useState([]);
@@ -16,54 +15,65 @@ function AdminDashboard({clicked, setClicked}) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const facultiesResponse = await axios.get('http://localhost:3001/getfacultyrecords');
+        const facultiesResponse = await axios.get(
+          "http://localhost:3001/getfacultyrecords"
+        );
         setFaculties(facultiesResponse.data);
 
-        const studentsResponse = await axios.get('http://localhost:3001/getstudentrecords');
+        const studentsResponse = await axios.get(
+          "http://localhost:3001/getstudentrecords"
+        );
         setStudents(studentsResponse.data);
 
-        const loginResponse = await axios.get('http://localhost:3001/getlogin');
+        const loginResponse = await axios.get("http://localhost:3001/getlogin");
         setlogin(loginResponse.data);
-
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
   }, []);
 
-  const generateloginaudit= async () => {
+  const generateloginaudit = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/generateloginaudit', {
-        responseType: 'blob',
-      });
+      const response = await axios.get(
+        "http://localhost:3001/generateloginaudit",
+        {
+          responseType: "blob",
+        }
+      );
 
       if (response.status === 200) {
         const blob = new Blob([response.data], {
-          type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+          type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         });
 
-        const downloadLink = document.createElement('a');
+        const downloadLink = document.createElement("a");
         downloadLink.href = window.URL.createObjectURL(blob);
-        downloadLink.setAttribute('download', 'LOGIN AND LOGOUT_AUDITLOG.xlsx');
+        downloadLink.setAttribute("download", "LOGIN AND LOGOUT_AUDITLOG.xlsx");
         document.body.appendChild(downloadLink);
         downloadLink.click();
         document.body.removeChild(downloadLink);
 
-        console.log('Student list audit log Excel file download initiated');
+        console.log("Student list audit log Excel file download initiated");
       } else {
-        console.error('Failed to initiate Student list audit log Excel file download');
+        console.error(
+          "Failed to initiate Student list audit log Excel file download"
+        );
       }
     } catch (error) {
-      console.error('Error while initiating Student list audit log Excel file download:', error);
+      console.error(
+        "Error while initiating Student list audit log Excel file download:",
+        error
+      );
     }
   };
 
   return (
     <main className="p-0 vh-100 w-100 position-relative">
       <section className="contatiner col-12 text-start  d-flex flex-column align-items-center justify-content-center">
-      <div className="d-flex w-100 align-items-center">
+        <div className="d-flex w-100 align-items-center">
           <div className="border-bottom border-dark py-1 ps-sm-0 ps-3">
             <i
               className="d-block d-sm-none bi bi-list fs-5 custom-red px-2 rounded "
@@ -73,35 +83,24 @@ function AdminDashboard({clicked, setClicked}) {
           <h2 className="px-2 w-100 border-bottom border-dark py-1 m-0 pe-sm-0 pe-3 text-center text-sm-start">
             ADMIN DASHBOARD
           </h2>
-        </div>  
+        </div>
         <section className="container-fluid col-12">
           <div className="row p-0 mt-2">
             <div className="col-6">
               <h2>WELCOME!</h2>
             </div>
-            <div className="col-6 d-flex flex-column gap-3 align-self-center ps-1 mb-2">
-              
-            </div>
-            <div className="col-6 pe-1 mb-2">
+            <div className="col-6 d-flex flex-column gap-3 align-self-center ps-1 mb-2"></div>
+
+            <div className="col-6 rounded ps-3 p-0 table-responsive custom-table-h">
               <span className="d-flex justify-content-between align-items-center bg-secondary rounded py-1 px-3">
                 RECENTLY REGISTERED
                 <Link href="/Admin_Page/Student_Page">
-                
-                <button className="btn btn-outline-light btn-sm border-secondary custom-black-color">
-                  GO TO STUDENT
-                </button>
+                  <button className="btn btn-outline-light btn-sm border-secondary custom-black-color">
+                    GO TO STUDENT
+                  </button>
                 </Link>
               </span>
-            </div>
-            <div className="col-6 ps-1">
-              <span className="d-flex justify-content-between align-items-center bg-secondary rounded py-1 px-3">
-                RECENTLY LOGGED IN/OUT
-                
-              </span>
-            </div>
-            
 
-            <div className="col-6 rounded pe-1 table-responsive custom-table-h">
               <table className="table-secondary table table-bordered border-secondary overflow-auto">
                 <thead>
                   <tr>
@@ -123,8 +122,11 @@ function AdminDashboard({clicked, setClicked}) {
                 </tbody>
               </table>
             </div>
-            
-            <div className="col-6 ps-1 table-responsive">
+
+            <div className="col-6 ps-1 table-responsive custom-table-h">
+              <span className="d-flex justify-content-between align-items-center bg-secondary rounded py-2 px-3">
+                RECENTLY LOGGED IN/OUT
+              </span>
               <table className="table table-secondary table-bordered border-secondary">
                 <thead>
                   <tr>
@@ -146,19 +148,16 @@ function AdminDashboard({clicked, setClicked}) {
                 </tbody>
               </table>
             </div>
-             <div className="col-6 ps-1">
+
+            <div className="col-6 p-0 ps-3 custom-table-h overflow-auto">
               <span className="d-flex justify-content-between align-items-center bg-secondary rounded py-1 px-3">
                 RECENTLY REGISTERED
                 <Link href="/Admin_Page/Faculty_Page">
-                <button className="btn btn-outline-light btn-sm border-secondary custom-black-color">
-                 GO TO FACULTY
-                </button>
+                  <button className="btn btn-outline-light btn-sm border-secondary custom-black-color">
+                    GO TO FACULTY
+                  </button>
                 </Link>
               </span>
-       
-
-            <div className="col-6 rounded pe-1 table-responsive custom-table-h">
-              </div>
               <table className="table-secondary table table-bordered border-secondary overflow-auto">
                 <thead>
                   <tr>
@@ -182,10 +181,13 @@ function AdminDashboard({clicked, setClicked}) {
             </div>
           </div>
           <section className="position-absolute bottom-0 end-0 mb-3 me-3 text-end">
-        <button className="btn btn-dark btn-lg" onClick={generateloginaudit} >
-          Download Login and Out Audit
-        </button>
-      </section>
+            <button
+              className="btn btn-dark btn-lg"
+              onClick={generateloginaudit}
+            >
+              Download Login and Out Audit
+            </button>
+          </section>
         </section>
       </section>
     </main>
